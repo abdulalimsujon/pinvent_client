3821d5ecdaab86b3018a06a21bbb68d1f8c027c0
abdulalimsujon <alimsujon12@gmail.com>
2023-01-03 23:53:32 +0600

    update

--- controllers/product.js
+++ controllers/product.js
@@ -42,6 +42,9 @@ const createProduct = asyncHandler(async (req, res) => {
 });
 
 
+//get all product
+
+
 const getAllProduct = asyncHandler(async (req, res) => {
 
     const products = await Product.find({ user: req.user.id }).sort("-createAt");
@@ -50,10 +53,112 @@ const getAllProduct = asyncHandler(async (req, res) => {
 
 })
 
+// get single product
 
+const singleProduct = asyncHandler(async (req, res) => {
+
+    const id = req.params.id;
+
+    const product = await Product.findById(id);
+    if (!product) {
+        res.status(404)
+        throw new Error('Product doesnt found')
+    }
+
+    // Match product to its user
+
+    if (product.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error("user not authorized")
+    }
+
+
+    res.status(200).json(product);
+
+
+})
+
+
+// delete the product 
+
+const deleteOne = asyncHandler(async (req, res) => {
+
+    const id = req.params.id;
+    const product = await Product.findById(id);
+    if (!product) {
+        res.status(404)
+        throw new Error('product not found')
+    }
+    if (product.user.toString() !== req.user.id) {
+
+        res.status(401)
+        throw new Error('Authentication failed')
+
+    }
+
+    await product.remove();
+
+    res.status(200).json({ message: 'delete successful' })
+})
+
+const updateProduct = asyncHandler(async (req, res) => {
+
+    const { name, sku, category, quantity, price, description } = req.body;
+
+    const id = req.params.id;
+
+    const product = await Product.findById(id);
+
+    if (!product) {
+        res.status(404)
+        throw new Error("product not found")
+    }
+
+    if (product.user.toString() !== req.user.id) {
+        res.status(401)
+        throw new Error('Authentication failed')
+    }
+
+    const filedata = {}
+
+    if (req.file) {
+        filedata = {
+            fileName: req.file.originalname,
+            filePath: req.file.path,
+            fileType: req.file.mimetype,
+            fileSize: req.file.size
+        }
+    }
+
+
+    const updateProduct = await Product.findByIdAndUpdate(
+        { _id: id },
+        {
+            name,
+            sku,
+            category,
+            quantity,
+            price,
+            description,
+            image: Object.keys(filedata).length === 0 ? product?.image : filedata,
+
+        },
+        {
+            new: true,
+            runValidators: true
+        }
+
+    )
+
+    res.status(200).json('product updated Successfully')
+
+})
 
 
 module.exports = {
     createProduct,
-    getAllProduct
+    getAllProduct,
+    singleProduct,
+    deleteOne,
+    updateProduct
 };
--- routes/product.js
+++ routes/product.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { createProduct, getAllProduct } = require('../controllers/product');
+const { createProduct, getAllProduct, singleProduct, deleteOne, updateProduct } = require('../controllers/product');
 
 const router = express.Router();
 
@@ -13,8 +13,12 @@ const protect = require('../middlewares/auth');
 
 
 
+
 router.post("/", protect, upload.single("image"), createProduct);
 router.get("/getallproducts", protect, getAllProduct);
+router.get("/singleproduct/:id", protect, singleProduct);
+router.delete("/deleteone/:id", protect, deleteOne);
+router.patch("/update/:id", upload.single("image"), protect, updateProduct);
 
 
 
